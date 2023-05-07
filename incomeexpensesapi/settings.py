3df7b898bacf3129c5892b594d7106a6fc8e4162
natnael340 <natnaeltilahun157@gmail.com>
2023-05-08 01:54:36 +0300

    removed secret key

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6dus6nw&#3$hg7fba$51&j^84#7ju6x&e01&6(x)e)_-@iu@t*"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
